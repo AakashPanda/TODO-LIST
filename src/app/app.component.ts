f1ef6eb0353f16ae94264769c6ca1c5ae745e1f4
Aakash panda <aakashpanda81@gmail.com>
2021-01-21 13:22:15 +0530

    Todo list commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
-@Component({
+import { Todo } from "./todo";
+
+@Component({                                                                      //Decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-Project';
+  title = 'Hii, I m Aakash Panda This is My first Angular JS Project ';
+
+  // date = Date()
+  // Name = 'It Works'
+
+
+  // constructor(){
+  //   console.log("It Called");
+  //   this.doSomething("this is Aakash")
+  // }
+
+  // doSomething(val : string): void {
+  //   val = "It was Awesome!";
+  // }
+
+  todoValue: string;
+  list: Todo[];
+
+  //Life Cycle Method
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo =
+      {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+
+      this.list.push(newItem);
+    }
+
+    this.todoValue = "";
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter
+      (
+        item => item.id !== id
+      );
+  }
+
 }
